d343f02848959fa02f8571447f5709ce6dabd6e5
saun2648 <ashikulislamsawan@gmail.com>
2022-10-13 07:04:11 +0600

    problem solved

--- lib/commons/collapsing_navigation_drawer_widget.dart
+++ lib/commons/collapsing_navigation_drawer_widget.dart
@@ -4,12 +4,17 @@ import 'package:flutter/material.dart';
 class CollapsingNavigationDrawer extends StatefulWidget {
   @override
   CollapsingNavigationDrawerState createState() {
-    return new CollapsingNavigationDrawerState();
+    return CollapsingNavigationDrawerState();
   }
 }
 
 class CollapsingNavigationDrawerState extends State<CollapsingNavigationDrawer>
     with SingleTickerProviderStateMixin {
+
+  List widgets = [const DashboardPage(), const ErorrsPage(), const SearchPage(), const NotificationsPage(), const SettingsPage()];
+
+  
+
   double maxWidth = 210;
   double minWidth = 70;
   bool isCollapsed = false;
@@ -21,7 +26,7 @@ class CollapsingNavigationDrawerState extends State<CollapsingNavigationDrawer>
   void initState() {
     super.initState();
     _animationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+        AnimationController(vsync: this, duration: const Duration(milliseconds: 300));
     widthAnimation = Tween<double>(begin: maxWidth, end: minWidth)
         .animate(_animationController);
   }
@@ -37,64 +42,118 @@ class CollapsingNavigationDrawerState extends State<CollapsingNavigationDrawer>
   Widget getWidget(context, widget) {
     return Material(
       elevation: 80.0,
-      child: Container(
-        width: widthAnimation.value,
-        color: drawerBackgroundColor,
-        child: Column(
-          children: <Widget>[
-            CollapsingListTile(
-              title: 'Techie',
-              icon: Icons.person,
-              animationController: _animationController,
-              onTap: () {},
-            ),
-            Divider(
-              color: Colors.grey,
-              height: 40.0,
-            ),
-            Expanded(
-              child: ListView.separated(
-                separatorBuilder: (context, counter) {
-                  return Divider(height: 12.0);
-                },
-                itemBuilder: (context, counter) {
-                  return CollapsingListTile(
-                    onTap: () {
-                      setState(() {
-                        currentSelectedIndex = counter;
-                      });
+      child: Row(
+        children: [
+          Container(
+            width: widthAnimation.value,
+            color: drawerBackgroundColor,
+            child: Column(
+              children: <Widget>[
+                CollapsingListTile(
+                  title: 'Techie',
+                  icon: Icons.person,
+                  animationController: _animationController,
+                  onTap: () {},
+                ),
+                const Divider(
+                  color: Colors.grey,
+                  height: 40.0,
+                ),
+                Expanded(
+                  child: ListView.separated(
+                    separatorBuilder: (context, counter) {
+                      return const Divider(height: 12.0);
                     },
-                    isSelected: currentSelectedIndex == counter,
-                    title: navigationItems[counter].title,
-                    icon: navigationItems[counter].icon,
-                    animationController: _animationController,
-                  );
-                },
-                itemCount: navigationItems.length,
-              ),
+                    itemBuilder: (context, counter) {
+                      return CollapsingListTile(
+                        onTap: () {
+                          setState(() {
+                            currentSelectedIndex = counter;
+                            
+                          });
+                        },
+                        isSelected: currentSelectedIndex == counter,
+                        title: navigationItems[counter].title,
+                        icon: navigationItems[counter].icon,
+                        animationController: _animationController,
+                      );
+                    },
+                    itemCount: navigationItems.length,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      isCollapsed = !isCollapsed;
+                      isCollapsed
+                          ? _animationController.forward()
+                          : _animationController.reverse();
+                    });
+                  },
+                  child: AnimatedIcon(
+                    icon: AnimatedIcons.close_menu,
+                    progress: _animationController,
+                    color: selectedColor,
+                    size: 50.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                ),
+              ],
             ),
-            InkWell(
-              onTap: () {
-                setState(() {
-                  isCollapsed = !isCollapsed;
-                  isCollapsed
-                      ? _animationController.forward()
-                      : _animationController.reverse();
-                });
-              },
-              child: AnimatedIcon(
-                icon: AnimatedIcons.close_menu,
-                progress: _animationController,
-                color: selectedColor,
-                size: 50.0,
-              ),
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-          ],
-        ),
+          ),
+          
+          widgets[currentSelectedIndex],
+          
+        ],
       ),
     );
+  }
+}
+
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Dashboard"),);
+  }
+}
+
+class ErorrsPage extends StatelessWidget {
+  const ErorrsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Erorrs"),);
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Search"),);
+  }
+}
+
+class NotificationsPage extends StatelessWidget {
+  const NotificationsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Notifications"),);
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Settings"),);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(
-      ),
+      home: MyHomePagea(),
     );
   }
 }
